b8e04df11140582183db84f556900cb9ff102b83
Cico <radualexandrucosmin@gmail.com>
2023-01-10 20:27:14 +0200

    Added manequin

---
+++ Assets/Characters/Mannequin/mannequiny.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$root/Skeleton3D.physical_bones_start_simulation();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
