4feb4f8098f23eff26a90852775adfa70b357ab5
Cico <radualexandrucosmin@gmail.com>
2023-01-11 16:10:19 +0200

    Show only two decimals, added Label3D

--- Mode/VirtualReality/Debug/VR_HandThumbstickDebug.gd
+++ Mode/VirtualReality/Debug/VR_HandThumbstickDebug.gd
@@ -1,17 +1,18 @@
-extends MeshInstance3D
+extends Label3D
 
 var controller: XRController3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		self.controller = self.get_parent()
+	self.controller = self.get_parent()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movement_vector = controller.get_axis("primary").normalized()
-	var textMeshIntance : TextMesh = self.mesh
+	var x = "%.2f" % movement_vector.x
+	var y = "%.2f" % movement_vector.y
 	# Define a format string with placeholder '%s'
 	var format_string = "%s %s"
 	# Using the '%' operator, the placeholder is replaced with the desired value
-	var actual_string = format_string % [movement_vector.x, movement_vector.y]	
-	textMeshIntance.set_text(actual_string)
+	var actual_string = format_string % [x, y]	
+	self.set_text(actual_string)
