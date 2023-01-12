b52c2ffe51310fe950cec8bd6bb15abdf88566d3
AlexRadu <radualexandrucosmin@gmail.com>
2023-01-12 22:37:21 +0200

    Extending Base

--- Mode/XRMode/PlayerBody/player_body.gd
+++ Mode/XRMode/PlayerBody/player_body.gd
@@ -1,9 +1,5 @@
 @tool
-extends CharacterBody3D
-
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+extends BaseCharacterBodyController
 
 @onready var origin_node = $XROrigin3D
 @onready var camera_node = $XROrigin3D/XRCamera3D
@@ -16,8 +12,6 @@ const JUMP_VELOCITY = 4.5
 		if is_inside_tree():
 			_update_head_position()
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _update_head_position():
 	head_position_node.transform.origin.z = eye_offset
@@ -79,18 +73,18 @@ func _process_on_player_input(delta):
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, move_speed)
+		velocity.z = move_toward(velocity.z, 0, move_speed)
 
 	move_and_slide()
 
