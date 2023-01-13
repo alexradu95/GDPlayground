6cfd1fb7924dcbc1b8d59c655bb8650faef0ec5d
AlexRadu <radualexandrucosmin@gmail.com>
2023-01-13 16:52:52 +0200

    Sync

--- Mode/XRMode/PlayerBody/player_body.gd
+++ Mode/XRMode/PlayerBody/player_body.gd
@@ -1,102 +1,30 @@
 @tool
 extends BaseCharacterBodyController
 
-@onready var origin_node = $XROrigin3D
-@onready var camera_node = $XROrigin3D/XRCamera3D
-@onready var head_position_node = $XROrigin3D/XRCamera3D/HeadPosition
-@onready var blackout_node = $XROrigin3D/XRCamera3D/BlackOut
-
-@export var eye_offset = 0.1:
-	set(new_value):
-		eye_offset = new_value
-		if is_inside_tree():
-			_update_head_position()
-
-func _ready():
-	_update_head_position()
-
-# Updates the head position
-func _update_head_position():
-	head_position_node.transform.origin.z = eye_offset
-	if Engine.is_editor_hint():
-		camera_node.transform.origin.z = -eye_offset
-
 func _physics_process(delta):
-	# Don't do this in editor
-	if Engine.is_editor_hint():
-		return
-
-	var colliding = _process_on_physical_movement(delta)
-	if !colliding: 
-		_process_on_player_input(delta)
-
-func _process_on_physical_movement(delta) -> bool:
-	# Remember our velocity we're not applying that on physical movement
-	var org_velocity = velocity
-
 	_sync_body_rotation_with_camera()
+		
+func _process_on_player_input(delta):
+	handle_jumping(delta)
+	handle_movement(delta)
 
-	# Now apply movement, first move our player body to the right location
-	var org_player_body : Vector3 = global_transform.origin
-	var player_body_location : Vector3 = origin_node.transform * camera_node.transform * head_position_node.transform.origin
-	player_body_location.y = 0.0
-	player_body_location = global_transform * player_body_location
-
-	velocity = (player_body_location - org_player_body) / delta
-	move_and_slide()
-
-	# Now move our XROrigin back
-	var delta_movement = org_player_body - global_transform.origin
-	delta_movement.y = 0.0
-	$XROrigin3D.global_transform.origin += delta_movement
-
-	# Return our value
-	velocity = org_velocity
-
-	# Adjust our fade
-	var distance : float = (player_body_location - org_player_body).length()
-	var colliding = distance > 0.01
-	if colliding:
-		var material : ShaderMaterial = blackout_node.get_surface_override_material(0)
-		if material:
-			var fade : float = clamp(distance * 10.0, 0.0, 1.0)
-
-			material.set_shader_parameter("fade", fade)
-		blackout_node.visible = true
-	else:
-		blackout_node.visible = false
-
-	return colliding
-
-# Start by rotating the player to face the same way our real player is
+# Rotates the player to face the same way our real player is
 func _sync_body_rotation_with_camera():
-		# Start by rotating the player to face the same way our real player is
-	var camera_basis : Basis = origin_node.transform.basis * camera_node.transform.basis
+	# Start by rotating the player to face the same way our real player is
+	# We retrieve camera_basis values
+	# This is how it's calculated. If you want to get to the global position of an object
+	# You will have to multiply parents until you get the Main node
+	# Basis is the 3x3 used for 3D rotation and scale
+	var camera_basis : Basis = $XROrigin3D.transform.basis * $XROrigin3D/XRCamera3D.transform.basis
+	# We care only about the forward direction that we are looking at
 	var forward : Vector2 = Vector2(camera_basis.z.x, camera_basis.z.z)
+	# We retrieve the angle between the forward direction and what?
 	var angle : float = forward.angle_to(Vector2(0.0, 1.0))
 	# print(angle)
 	
-	transform.basis = transform.basis.rotated(Vector3.UP, angle)
-	origin_node.transform = Transform3D().rotated(Vector3.UP, -angle) * origin_node.transform
+	# We rotate the character to the same angle that our camera rotated
+	transform = transform.rotated(Vector3.UP, angle)
 
-func _process_on_player_input(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	# We rotate the XROrigin3D in the oposite direction, in order to keep it's correct position
+	$XROrigin3D.transform = $XROrigin3D.transform.rotated(Vector3.UP, -angle)
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * move_speed
-		velocity.z = direction.z * move_speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, move_speed)
-		velocity.z = move_toward(velocity.z, 0, move_speed)
-
-	move_and_slide()
