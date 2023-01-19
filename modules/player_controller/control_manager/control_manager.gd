a913b10cfd28f9fcb83494af15ecec89076fd065
Cico <radualexandrucosmin@gmail.com>
2023-01-19 23:01:13 +0200

    Implemented minimum WebXR

--- modules/player_controller/control_manager/control_manager.gd
+++ modules/player_controller/control_manager/control_manager.gd
@@ -23,23 +23,25 @@ func _ready() -> void:
 # Perform OpenXR setup
 func _initialize_openxr():
 	print("OpenXR: Configuring interface")
-	var xr_interface = XRServer.find_interface('OpenXR')
+	var xr_interface : OpenXRInterface = XRServer.find_interface('OpenXR')
+	if xr_interface:
+		# Initialize the OpenXR interface
+		if not xr_interface.is_initialized():
+			print("OpenXR: Initializing interface")
+			if xr_interface.initialize():
+				supportedXR = XRType.OPENXR
+				$Panel/HBoxContainer/XR.text = "Play OpenXR"
+			else:
+				push_error("OpenXR: Failed to initialize")
+	else:
+		print("OpenXR: Could not find OpenXR Interface")
 
-	# Initialize the OpenXR interface
-	if not xr_interface.is_initialized():
-		print("OpenXR: Initializing interface")
-		if not xr_interface.initialize():
-			push_error("OpenXR: Failed to initialize")
-			return false
-			
-	supportedXR = XRType.OPENXR
-	$Panel/HBoxContainer/XR.text = "Play OpenXR"
-	
 func _initialize_webxr():
 	print("WebXR: Configuring interface")
-	var webxr_interface = XRServer.find_interface("WebXR")
+	var webxr_interface : WebXRInterface = XRServer.find_interface("WebXR")
 	
 	if webxr_interface:
+		print("WebXR: webxr_interface found")
 		# Map to the standard button/axis ids when possible.
 		webxr_interface.xr_standard_mapping = true
 		# WebXR uses a lot of asynchronous callbacks, so we connect to various signals in order to receive them.
@@ -52,6 +54,8 @@ func _initialize_webxr():
 		# (which we connected to the "session_supported" signal above) will
 		# be called sometime later to let us know if it's supported or not.
 		webxr_interface.is_session_supported("immersive-vr")
+	else:
+		print("WebXR: Could not find OpenXR Interface")
 		
 func _on_webxr_session_supported(session_mode: String, supported: bool) -> void:
 	if session_mode == "immersive-vr":
@@ -59,6 +63,7 @@ func _on_webxr_session_supported(session_mode: String, supported: bool) -> void:
 			supportedXR = XRType.WEBXR
 			$Panel/HBoxContainer/XR.text = "Play WebXR"
 		else:
+			print("WebXR: Failed to configure interface even if thought webXr interface is present")
 			$Panel/HBoxContainer/XR.text = "The browser does not support WebXR"
 			$Panel/HBoxContainer/XR.disabled = true
 
@@ -76,18 +81,26 @@ func launch_xr_button_pressed():
 	
 	hide()
 	
-	var vp : Viewport = get_viewport()
-	vp.use_xr = true
-	
 	if(supportedXR == XRType.WEBXR):
+		# We want an immersive VR session, as opposed to AR ('immersive-ar') or a
+		# simple 3DoF viewer ('viewer').
 		webxr_interface.session_mode = 'immersive-vr'
+		# 'bounded-floor' is room scale, 'local-floor' is a standing or sitting
+		# experience (it puts you 1.6m above the ground if you have 3DoF headset),
+		# whereas as 'local' puts you down at the XROrigin.
+		# This list means it'll first try to request 'bounded-floor', then
+		# fallback on 'local-floor' and ultimately 'local', if nothing else is
+		# supported.
 		webxr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
+		# In order to use 'local-floor' or 'bounded-floor' we must also
+		# mark the features as required or optional.
 		webxr_interface.required_features = 'local-floor'
 		webxr_interface.optional_features = 'bounded-floor'
 		if not webxr_interface.initialize():
 			OS.alert("Failed to initialize WebXR")
+			
+	var vp : Viewport = get_viewport()
+	vp.use_xr = true
 
 	self.launch_xr.emit()
-
-	
 
