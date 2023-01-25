f483a848ebcca9077a366d21c28c04141d40b0fd
Cico <radualexandrucosmin@gmail.com>
2023-01-26 00:33:56 +0200

    GD Beta 15

--- addons/gridmap_noise/gridmap_noise.gd
+++ addons/gridmap_noise/gridmap_noise.gd
@@ -1,58 +1,72 @@
 @tool
-extends GridMap
+extends Node3D
+
+@onready var waterMesh: MeshInstance3D = $Water
+@onready var treeMultiMesh: MultiMeshInstance3D = $TreeMultiMesh
+@onready var gridMap: GridMap = $GridMap
 
 
 @export var noise: FastNoiseLite = FastNoiseLite.new()
+@export var size: int = 200
+@export var marginSize: int = 4 
 
-@export var grid_size: int = 200 :
+var instancedTrees = 0
+
+@export var generate: bool = false :
 	set(value):
-		grid_size = value
-		generate_terrain_gridmap(grid_size)
-
-@onready var increment = self.mesh_library.get_item_list().size();
-
-
+		if Engine.is_editor_hint():
+			generate_terrain_gridmap(size)
+			generate = value
+		
+@export var clean: bool = false :
+	set(value):
+		if Engine.is_editor_hint():
+			cleanUp()
+			clean = value
+		
+func cleanUp():
+	gridMap.clear()
+	treeMultiMesh.multimesh.instance_count = 0
 
 func _ready():
-	generate_terrain_gridmap(grid_size)
-	
-func generate_terrain_gridmap(size: int):
-	clear()
+	generate_terrain_gridmap(size)
 
-	var treshholds = generate_thresholds()
-	var i = 0
+func generate_terrain_gridmap(size: int):
+	cleanUp()
 	
-	var multiMeshInstance : MultiMeshInstance3D = $TreeMultiMesh
-	$TreeMultiMesh.multimesh.instance_count = grid_size * 100
+	treeMultiMesh.multimesh.instance_count = size * size
+	
+	if(waterMesh):
+		waterMesh.mesh.size = Vector3(size+1, 3, size+1)
+	
+	var treshholds = generate_thresholds()
+
 	
 	for y in range(size):
 		for x in range(size):
 			var vector = Vector3(x, 0, y)
-			var height : int = calculate_height_based_on_color(vector, treshholds)
-			vector.y = height
-						
-			if $TreeMultiMesh:
-				if i < $TreeMultiMesh.multimesh.instance_count && ( height == 5 || height == 6 || height == 7 ) && noise.get_noise_3dv(vector) > 0.15:
-					$TreeMultiMesh.multimesh.set_instance_transform(i, Transform3D(Basis.IDENTITY, 
-						vector + Vector3.RIGHT/2  + Vector3.BACK/2+ Vector3.UP))
-					i = i+1
-					$TreeMultiMesh.multimesh.visible_instance_count = i
-					
-			set_cell_item(vector, height)
+			var eval = self.evaluate(vector, treshholds)
+			
+			if y <= marginSize-1 || x <= marginSize-1 || y >= size-marginSize || x >= size-marginSize :
+				eval = 1
+				self.gridMap.set_cell_item(vector + Vector3.UP, -1)
+				self.gridMap.set_cell_item(vector + 2 * Vector3.UP, -1)
+				self.gridMap.set_cell_item(vector + 3 * Vector3.UP, -1)
+			
+			vector.y = eval
 
-func generate_thresholds() -> Array[float]:
+			if treeMultiMesh:
+				if instancedTrees < treeMultiMesh.multimesh.instance_count && eval == $GridMap.mesh_library.get_item_list().size() / 2 && noise.get_noise_3dv(vector) > 0.15:
+					treeMultiMesh.multimesh.set_instance_transform(instancedTrees, Transform3D(Basis.IDENTITY, 
+						vector + Vector3.RIGHT/2  + Vector3.BACK/2+ Vector3.UP))
+					instancedTrees += 1
+					
+			self.gridMap.set_cell_item(vector, eval)
 	
-	var meshLibrarySize : float = self.mesh_library.get_item_list().size()
-	increment = 2 /  meshLibrarySize
-	var thresholds : Array[float];
+	#if treeMultiMesh:
+	#	treeMultiMesh.multimesh.visible_instance_count = instancedTrees
 	
-	for i in meshLibrarySize + 1:
-		var value = -1 + i * increment
-		thresholds.append(round_to_dec(value, 2))
-	
-	return thresholds
-	
-func calculate_height_based_on_color(positon: Vector3, treshholds: Array[float]) -> int:
+func evaluate(positon: Vector3, treshholds: Array[float]) -> int:
 	# Height can be from -1 to +1
 	var eval : float = noise.get_noise_2d(positon.x, positon.z)
 	var discreteValue = round_to_dec(eval, 2)
@@ -60,11 +74,18 @@ func calculate_height_based_on_color(positon: Vector3, treshholds: Array[float])
 		if(discreteValue < treshholds[i]):
 			return i
 	return 0
+
+func generate_thresholds() -> Array[float]:
 	
+	var meshLibrarySize : float = $GridMap.mesh_library.get_item_list().size()
+	var increment = 2 /  meshLibrarySize
+	var thresholds : Array[float];
+	
+	for i in meshLibrarySize + 1:
+		var value = -1 + i * increment
+		thresholds.append(round_to_dec(value, 2))
+	
+	return thresholds
+
 func round_to_dec(num, digit):
 	return round(num * pow(10.0, digit)) / pow(10.0, digit)
-
-
-################################################ OLD METHOD ##############################
-
-	
